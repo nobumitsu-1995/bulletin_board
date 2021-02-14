4f1a12a254e58474c79abb6cee3a7d062abda7b7
nobumisu-1995 <masugata1995@mailcom>
2021-02-14 11:20:33 +0900

    repair omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], request_path: '/auth/twitter', callback_path: '/auth/twitter/callback'
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 end
