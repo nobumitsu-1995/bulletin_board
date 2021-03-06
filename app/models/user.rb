7b544202affc3b35cec13555d5ba17312a2df032
masugata1995 <masugata1995@gmail.com>
2021-03-06 22:04:50 +0900

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+  has_one_attached :image
   validates :password_digest, presence: true
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
